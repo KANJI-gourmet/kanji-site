8d2179a5a67b9c7eeef1f69959946be0aa3373a6
KANJI-gourmet <kofinkouta@gmail.com>
2026-09-24 00:29:46 +0900

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.KANJI_CONFIG = {
+  SUPABASE_URL: "https://lbennjlvyyasdzhsczru.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_rxCtGrnlfsWVEieG42um2g_w6xZE9he",
+  GA_MEASUREMENT_ID: "PASTE_GA4_MEASUREMENT_ID"
+};

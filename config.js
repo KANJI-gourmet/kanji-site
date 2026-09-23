f49cb9690f6a96f6bf0a2972502f5a7d6811be44
KANJI-gourmet <kofinkouta@gmail.com>
2026-09-24 01:03:03 +0900

    Fix config.js for member auth

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.KANJI_CONFIG = {
   SUPABASE_URL: "https://lbennjlvyyasdzhsczru.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_rxCtGrnlfsWVEieG42um2g_w6xZE9he",
+  SITE_URL: "https://kanji-gourmet.github.io/kanji-site/",
   GA_MEASUREMENT_ID: "PASTE_GA4_MEASUREMENT_ID"
 };
